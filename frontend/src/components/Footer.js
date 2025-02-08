00cd354c2acc0ad5c2f17d89a020607c1abbf301
Tobiasz-T <96210491+Tobiasz-T@users.noreply.github.com>
2025-02-08 23:15:03 +0100

    pliki

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import '../App.css';
+
+class Footer extends Component{
+    render(){
+        return(
+            <div id="footer">
+                <a href="">link</a>
+            </div>
+        );
+    }
+}
+
+export default Footer;
