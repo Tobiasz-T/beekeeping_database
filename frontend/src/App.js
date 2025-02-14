aa9410af65fcbf0c91a548357beabc37bcd23888
Tobiasz-T <96210491+Tobiasz-T@users.noreply.github.com>
2025-02-14 13:14:28 +0100

    podstrony

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from './pages/Home';
+import Form from './pages/Form';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Header/>
       <header className="App-header">
-        
+        <>
+          
+            <Routes>
+              <Route exact path='/' element={<Home/>}/>
+              <Route exact path='/form' element={<Form/>}/>
+            </Routes>
+          
+        </>
       </header>
+      </BrowserRouter>
       <Footer/>
     </div>
   );
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../App.css';
+import { NavLink } from 'react-router-dom';
 
 function Head(){
     return (
@@ -7,7 +8,10 @@ function Head(){
         <div id="titlle">
             <h1>Beekeeping Database</h1>
         </div>
-        <div id="nav">nav</div>
+        <div id="nav">
+            <NavLink to='/' className="nav-link">Home</NavLink>
+            <NavLink to='/form' className="nav-link">Form</NavLink>
+        </div>
     </div>
     );
 }
